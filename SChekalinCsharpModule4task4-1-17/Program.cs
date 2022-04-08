247e18274a62af9d2977cf3888e1d8318fc7145c
Сергей Чекалин <schekalin@mail.ru>
2022-04-08 19:55:57 +0300

    Final Task

--- SChekalinCsharpModule4task4-1-17/Program.cs
+++ SChekalinCsharpModule4task4-1-17/Program.cs
@@ -4,7 +4,7 @@ namespace SChekalinCsharpModule4task4_1_17
 {
 	class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
 			var arr = new int[] {5,6,9,1,2,3,4};
 			var sum = 0;
