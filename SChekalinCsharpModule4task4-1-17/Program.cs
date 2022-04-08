c05e3078ff7e5002e47d1af775121f2242c5da18
Сергей Чекалин <schekalin@mail.ru>
2022-04-08 18:37:54 +0300

    Task 4.3.7.

--- SChekalinCsharpModule4task4-1-17/Program.cs
+++ SChekalinCsharpModule4task4-1-17/Program.cs
@@ -6,41 +6,10 @@ namespace SChekalinCsharpModule4task4_1_17
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Напишите свой любимый текст с маленькой буквы");
-			for (int i = 1; i < 5; i++)
-			{
-				Console.WriteLine($"Iteration {i}");
-
-				switch (Console.ReadLine())
-				{
-					case "red":
-						Console.BackgroundColor = ConsoleColor.Red;
-						Console.ForegroundColor = ConsoleColor.Black;
-
-						Console.WriteLine("Your color is red!");
-						break;
-
-					case "green":
-						Console.BackgroundColor = ConsoleColor.Green;
-						Console.ForegroundColor = ConsoleColor.Black;
-
-						Console.WriteLine("Your color is green!");
-						break;
-					case "cyan":
-						Console.BackgroundColor = ConsoleColor.Cyan;
-						Console.ForegroundColor = ConsoleColor.Black;
-
-						Console.WriteLine("Your color is cyan!");
-						break;
-
-					default:
-						Console.BackgroundColor = ConsoleColor.Yellow;
-						Console.ForegroundColor = ConsoleColor.Red;
-
-						Console.WriteLine("Your color is yellow!");
-						break;
-				}
-			}
+			Console.Write("Введите свое имя:");
+			string name = Console.ReadLine();
+			for (int i = name.Length - 1; i >=0; i--)
+				Console.Write($"{name[i]} ");
 		}
 	}
 }
